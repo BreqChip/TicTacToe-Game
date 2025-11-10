a418f7d7295e5ff6aee081113a09a17756841c31
Nisan <nchiproo@asu.edu>
2025-11-10 12:00:32 -0500

    Add initial TicTacToe class with main method

---
+++ src/TicTacToe.java
@@ -0,0 +1,9 @@
+public class TicTacToe {
+public static void main(String[] args) {
+cat > src/TicTacToe.java << 'EOF'
+public class TicTacToe {
+public static void main(String[] args) {
+System.out.print("Welcome to TicTacToe!");
+//TODO: Implement game logic
+}
+}
